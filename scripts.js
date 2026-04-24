a4575bd923a3f7e567b81b2540d7a1c0a8a65294
CLEMENSound <clemensound@naver.com>
2026-04-24 13:40:47 +0900

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// 스크롤 부드럽게 이동
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
