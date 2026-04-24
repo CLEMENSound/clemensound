e6ef7205c7c5ad1320f7a86038b15358437d8b5f
CLEMENSound <clemensound@naver.com>
2026-04-24 20:17:12 +0900

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// 스크롤 부드럽게 이동
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
-  });
-});
